cf0450528f31e13a2c7335d2676e1ab370e1a6c9
unryeong <leyng7@gmail.com>
2021-06-01 23:42:15 +0900

    순수 JDBC

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 `java -jar hello-spring-0.0.1-SNAPSHOT.jar` 로 서버를 실행할 수 있다.
 
 `./gradlew clean` 으로 빌드 폴더를 삭제할 수 있다.
+
+### H2 설치
+
+`brew install h2` 로 설치 후 `h2` 로 실행
+
+`jdbc:h2:~/test` (최초 한번)
+
+`~/test.mv.db` 파일 생성 확인
+
+`jdbc:h2:tcp://localhost/~/test` 이후에는 이렇게 접속
--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -2,7 +2,6 @@ package hello.hellospring.controller;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +14,6 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
